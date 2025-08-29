9adda15e727d6bf058a9544e6a3662f7b8e78da8
Nahid Hasan <146347290+alimnh520@users.noreply.github.com>
2025-08-29 20:19:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ setInterval(() => {
 
 
 const helloText = "I♡Love♡You";
-const nameText = "Sameya";
+const nameText = "MAYA";
 
 // Function to animate text with a delay
 function animateText(text, containerId, delay = 0) {
